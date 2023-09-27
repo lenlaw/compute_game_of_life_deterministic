f4d645d24989e9666a6fb1dd0593709c902b939f
lenlaw <glhoward33@hotmail.co.uk>
2023-09-27 20:57:56 +0100

    added another texture as texture_read and changed the existing one to texture_write
    i wanted a run, which i have and that made me do some clunky stuff
    like call the pixel create fn rather than clone the original image
    so ill go back and tidy up after this commit

--- src/compute_shader_game_of_life.rs
+++ src/compute_shader_game_of_life.rs
@@ -109,7 +109,7 @@ fn setup(mut commands: Commands, mut images: ResMut<Assets<Image>>) {
 
    
     //get a vec<u8> to populate the image pixel field with
-    let pixel_data = initial_image_pixels();
+    let pixel_data_write = initial_image_pixels();
 
     let mut image_write = Image::new(
         Extent3d {
@@ -118,7 +118,7 @@ fn setup(mut commands: Commands, mut images: ResMut<Assets<Image>>) {
             depth_or_array_layers: 1,
         },
         TextureDimension::D2,
-        pixel_data,
+        pixel_data_write,
         TextureFormat::Rgba8Unorm,
     );
 
@@ -153,8 +153,19 @@ DONE -how to copy one text to the other
 //so perhaps better to duplicate the simage created above espaecially since
 // this is going to need to be like the above and not empty 
 //as it has the initial conditions
-    let mut image_read  = image_write.clone();
-   
+//
+   // let mut image_read  = image_write.clone();
+   let pixel_data_read = initial_image_pixels();
+    let mut image_read = Image::new(
+        Extent3d {
+            width: SIZE.0,
+            height: SIZE.1,
+            depth_or_array_layers: 1,
+        },
+        TextureDimension::D2,
+        pixel_data_read,
+        TextureFormat::Rgba8Unorm,
+    );
 
     //gpt: the texture usages are apparently binary values ie 0b000010
     // and the | operator combines them bitwise so we might get sommet like
@@ -162,29 +173,46 @@ DONE -how to copy one text to the other
     image_write.texture_descriptor.usage =
         TextureUsages::COPY_DST | TextureUsages::STORAGE_BINDING | TextureUsages::TEXTURE_BINDING;
    
+
+    image_read.texture_descriptor.usage =
+    TextureUsages::COPY_DST | TextureUsages::STORAGE_BINDING | TextureUsages::TEXTURE_BINDING;
+
+
    //Handles are lightweight IDs that refer to a specific asset. You need them to use your assets, 
    //You can store handles in your entity components or resources.
    //Handles can refer to not-yet-loaded assets, meaning you can just spawn your entities anyway, 
    //using the handles, and the assets will just "pop in" when they become ready.
    /*Certainly, I can explain why images.add(image) returns a Handle<Image> and why it's used in Bevy.
 
-In Bevy, asset management is a crucial part of game development. The images variable you have is of type Assets<Image>, which represents a collection of assets of type Image. In Bevy, assets like textures or images are managed separately from regular collections like arrays or vectors. Instead of directly adding an image to a collection, Bevy uses a system of handles to manage assets efficiently.
+In Bevy, asset management is a crucial part of game development. The images variable you have is of type Assets<Image>,
+ which represents a collection of assets of type Image. In Bevy, assets like textures or images are managed separately 
+ from regular collections like arrays or vectors. Instead of directly adding an image to a collection,
+  Bevy uses a system of handles to manage assets efficiently.
 
 Here's how it works:
 
-images.add(image): This method is used to add an image to the asset manager (Assets<Image>). However, it doesn't simply add the image to an array; it returns a Handle<Image> instead.
+images.add(image): This method is used to add an image to the asset manager (Assets<Image>). 
+However, it doesn't simply add the image to an array; it returns a Handle<Image> instead.
 
-Handle<Image>: This is a reference or a handle to the image asset stored in the asset manager. It's not the actual image data; it's more like a pointer or a reference to that data.
+Handle<Image>: This is a reference or a handle to the image asset stored in the asset manager.
+ It's not the actual image data; it's more like a pointer or a reference to that data.
 
 Why does Bevy use handles instead of directly adding to a collection?
 
-Efficiency: Bevy can perform various optimizations when managing assets using handles. For example, it can efficiently load, unload, and track dependencies between assets.
+Efficiency: Bevy can perform various optimizations when managing assets using handles.
+ For example, it can efficiently load, unload, and track dependencies between assets.
 
-Safety: Using handles ensures that the lifetime and ownership of assets are managed properly. It prevents issues like dangling references or assets being accidentally modified from multiple places.
+Safety: Using handles ensures that the lifetime and ownership of assets are managed properly.
+ It prevents issues like dangling references or assets being accidentally modified from multiple places.
 
-So, when you write let image = images.add(image);, you are adding the image to the asset manager and getting a handle to it. This handle (image) allows you to refer to and use the image within your Bevy application while ensuring that Bevy can manage it efficiently and safely behind the scenes.
+So, when you write let image = images.add(image);, you are adding the image to the asset manager and getting a handle to it. 
+This handle (image) allows you to refer to and use the image within your Bevy application 
+while ensuring that Bevy can manage it efficiently and safely behind the scenes.
 
-In summary, the Handle<Image> returned by images.add(image) is a way for Bevy to manage and optimize the loading and use of assets like images in a game or application. It's a design choice made to improve performance and maintain code safety. */
+In summary, the Handle<Image> returned by images.add(image) is a way for Bevy to 
+manage and optimize the loading and use of assets like images in a game or application.
+ It's a design choice made to improve performance and maintain code safety. */
+
     //the above comment is expandable
     let image_write = images.add(image_write);
     let image_read = images.add(image_read);
@@ -225,7 +253,7 @@ impl Plugin for GameOfLifeComputePlugin {
         //        <<theres no reference tho ..how do we use this extracted resource?
         //
         app.add_plugins(ExtractResourcePlugin::<GameOfLifeImageWrite>::default());
-
+        app.add_plugins(ExtractResourcePlugin::<GameOfLifeImageRead>::default());
         //so i think here we are getting the render half of the bevy engine 
         //(as opposed to the world half that owns the resources n stuff)
         //
@@ -275,7 +303,10 @@ struct GameOfLifeImageWrite(Handle<Image>);
 #[derive(Resource, Clone, Deref, ExtractResource)]
 struct GameOfLifeImageRead(Handle<Image>);
 
-
+/*
+ panicked at 'Resource requested by gol_compute::compute_shader_game_of_life::queue_bind_group 
+              does not exist: gol_compute::compute_shader_game_of_life::GameOfLifeImageRead'
+*/
 
 #[derive(Resource)]
 struct GameOfLifeImageBindGroup(BindGroup);
@@ -284,16 +315,21 @@ fn queue_bind_group(
     mut commands: Commands,
     pipeline: Res<GameOfLifePipeline>,
     gpu_images: Res<RenderAssets<Image>>,
-    game_of_life_image: Res<GameOfLifeImageWrite>,
+    game_of_life_image_write: Res<GameOfLifeImageWrite>,
+    game_of_life_image_read: Res<GameOfLifeImageRead>,
     render_device: Res<RenderDevice>,
 ) {
-    let view = &gpu_images[&game_of_life_image.0];
+
+
+    let view_write = &gpu_images[&game_of_life_image_write.0];
+    let view_read = &gpu_images[&game_of_life_image_read.0];
+
     let bind_group = render_device.create_bind_group(&BindGroupDescriptor {
         label: None,
         layout: &pipeline.texture_bind_group_layout,
         entries: &[BindGroupEntry {
             binding: 0,
-            resource: BindingResource::TextureView(&view.texture_view),
+            resource: BindingResource::TextureView(&view_write.texture_view),
         }],
     });
 
